57125d00c9f3c83a88b038a8b0332ea9e0dc2e02
Lavindeep Dhillon <lavindeepdhillon@gmail.com>
2026-05-11 10:56:13 -0400

    feat(mixin): add isRendererPathActive guards to WindowMixins (not yet enabled)

--- src/main/java/com/radiance/mixins/vulkan_render_integration/WindowMixins.java
+++ src/main/java/com/radiance/mixins/vulkan_render_integration/WindowMixins.java
@@ -3,12 +3,15 @@ package com.radiance.mixins.vulkan_render_integration;
 import static org.lwjgl.glfw.GLFW.GLFW_CLIENT_API;
 import static org.lwjgl.glfw.GLFW.GLFW_NO_API;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.radiance.client.RadianceState;
 import com.radiance.client.proxy.vulkan.WindowProxy;
 import net.minecraft.client.WindowEventHandler;
 import net.minecraft.client.WindowSettings;
 import net.minecraft.client.util.MonitorTracker;
 import net.minecraft.client.util.Window;
 import org.lwjgl.glfw.GLFW;
+import org.lwjgl.opengl.GL;
 import org.lwjgl.opengl.GLCapabilities;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -31,7 +34,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 0), remap = false)
     public void cancelGLFWWindowHint0(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -40,7 +46,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 1, remap = false))
     public void cancelGLFWWindowHint1(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -49,7 +58,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 2, remap = false))
     public void cancelGLFWWindowHint2(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -58,7 +70,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 3, remap = false))
     public void cancelGLFWWindowHint3(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -67,7 +82,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 4, remap = false))
     public void cancelGLFWWindowHint4(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -76,7 +94,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwWindowHint(II)V", ordinal = 5, remap = false))
     public void cancelGLFWWindowHint5(int hint, int value) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwWindowHint(hint, value);
+            return;
+        }
     }
 
     @Inject(method =
@@ -94,6 +115,7 @@ public class WindowMixins {
         String fullscreenVideoMode,
         String title,
         CallbackInfo ci) {
+        if (!RadianceState.isRendererPathActive()) return;
         GLFW.glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     }
 
@@ -103,7 +125,10 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwMakeContextCurrent(J)V", remap = false))
     public void cancelGLFWMakeContextCurrent(long window) {
-
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwMakeContextCurrent(window);
+            return;
+        }
     }
 
     @Redirect(method =
@@ -114,6 +139,9 @@ public class WindowMixins {
             target = "Lorg/lwjgl/opengl/GL;createCapabilities()Lorg/lwjgl/opengl/GLCapabilities;",
             remap = false))
     public GLCapabilities cancelGLCreateCapabilities() {
+        if (!RadianceState.isRendererPathActive()) {
+            return GL.createCapabilities();
+        }
         return null;
     }
 
@@ -123,6 +151,9 @@ public class WindowMixins {
             "Lnet/minecraft/client/WindowSettings;Ljava/lang/String;Ljava/lang/String;)V",
         at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/systems/RenderSystem;maxSupportedTextureSize()I", remap = false))
     public int cancelGetMaxSupportedTextureSize() {
+        if (!RadianceState.isRendererPathActive()) {
+            return RenderSystem.maxSupportedTextureSize();
+        }
         return 0;
     }
 
@@ -133,12 +164,17 @@ public class WindowMixins {
         at = @At(value = "INVOKE", target = "Lorg/lwjgl/glfw/GLFW;glfwSetWindowSizeLimits(JIIII)V", remap = false))
     public void cancelGetMaxSupportedTextureSize(long window, int minwidth, int minheight,
         int maxwidth, int maxheight) {
+        if (!RadianceState.isRendererPathActive()) {
+            GLFW.glfwSetWindowSizeLimits(window, minwidth, minheight, maxwidth, maxheight);
+            return;
+        }
         // Don't allow user to set window size manually
     }
 
     @Inject(method = "onFramebufferSizeChanged(JII)V",
         at = @At(value = "INVOKE", target = "Lnet/minecraft/client/WindowEventHandler;onResolutionChanged()V"))
     public void framebufferSizeChanged(long window, int width, int height, CallbackInfo ci) {
+        if (!RadianceState.isRendererPathActive()) return;
         WindowProxy.onFramebufferSizeChanged();
     }
 
